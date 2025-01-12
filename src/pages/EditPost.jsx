911c30736eac7a482a4e2f33e1bdc84f6dde4458
Mohd Ashraf Malik <ashraf336786@gmail.com>
2025-01-13 03:00:42 +0530

    Update EditPost.jsx

--- src/pages/EditPost.jsx
+++ src/pages/EditPost.jsx
@@ -1,5 +1,5 @@
 import React, {useEffect,useState} from 'react'
-import { Container, PostForm } from '../components/index'
+import { Container, PostForm } from '../components'
 import { useNavigate, useParams } from 'react-router-dom'
 import appwriteServices from '../appwrite/config'
 
